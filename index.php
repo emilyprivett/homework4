d3f6cba20a94948de8d7b9b2c1cd86ab0130475d
Emily Privett <eaprivett@ou.edu>
2022-10-17 14:12:44 -0500

    added title

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ if ($conn->connect_error) {
 }
 ?>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Homework 3</a>
+  <a class="navbar-brand" href="#">Homework 4</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -49,7 +49,7 @@ if ($conn->connect_error) {
     </ul>
   </div>
 </nav>
-    <h1 style="text-align:center;">Homework 3: Databases!!</h1>
+    <h1 style="text-align:center;">Homework 4: Data Modification Functions!</h1>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
   </body>
