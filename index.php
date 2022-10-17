f74ca8f70252c871654308f6dfe2ed8a7f3ebce5
Emily Privett <eaprivett@ou.edu>
2022-10-17 13:57:15 -0500

    jdnfjlsdfn

---
+++ index.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Homework 3</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+  </head>
+  <body>
+<?php
+$servername = "localhost";
+$username = "emilypri_homework3";
+$password = "h0mework_3";
+$dbname = "emilypri_firstdatabase";
+ 
+// Create connection 
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+?>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Homework 3</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home</a>
+      </li>
+            <li class="nav-item">
+        <a class="nav-link" href="professor.php">Professors</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="course.php">Courses</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="student.php">Students</a>
+      </li>
+            <li class="nav-item">
+        <a class="nav-link" href="school.php">Schools</a>
+      </li>
+                  <li class="nav-item">
+        <a class="nav-link" href="studentschool.php">Student & School</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+    <h1 style="text-align:center;">Homework 3: Databases!!</h1>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ professorcourse.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Homework 3</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Homework 3</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home</a>
+      </li>
+            <li class="nav-item">
+        <a class="nav-link" href="professor.php">Professors</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="course.php">Courses</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="student.php">Students</a>
+      </li>
+            <li class="nav-item">
+        <a class="nav-link" href="school.php">Schools</a>
+      </li>
+         <li class="nav-item">
+        <a class="nav-link" href="studentschool.php">Student & School</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+    <h1 style="text-align:center;">Professor Courses</h1>
+    <table class="table table-primary">
+  <thead>
+    <tr>
+      <th>Course Name</th>
+      <th>Course ID</th>
+    </tr>
+  </thead>
+  <tbody>
+<?php
+$servername = "localhost";
+$username = "emilypri_homework3";
+$password = "h0mework_3";
+$dbname = "emilypri_firstdatabase";
+ 
+// Create connection 
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+$pid=$_POST['id'];
+$sql = "SELECT CourseName, CourseID FROM Course c JOIN Professor p ON c.ProfessorID=p.ProfessorID WHERE p.ProfessorID=" .$pid;
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+?>
+   <tr>
+    <td><?=$row["CourseName"]?></td>
+    <td><?=$row["CourseID"]?></td>
+  </tr>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+  </tbody>
+   </table>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+  </body>
+</html>
