ad97eb4e686ada5fcea27c9e965c1a6f66dbd248
Emily Privett <eaprivett@ou.edu>
2022-10-25 17:18:19 -0500

    work

--- professor.php
+++ professor.php
@@ -155,13 +155,7 @@ if ($result->num_rows > 0) {
 
     </td>
   </tr>
-<?php
-  }
-} else {
-  echo "0 results";
-}
-$conn->close();
-?>
+
   </tbody>
     </table>
   <br />
@@ -206,6 +200,13 @@ $conn->close();
 
     <br />
      
+    <?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
   </body>
 </html>
