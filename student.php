0d2d692d971badf7f650bd32e90f69dab0eec5e6
Emily Privett <eaprivett@ou.edu>
2022-10-24 21:20:24 -0500

    try again

--- student.php
+++ student.php
@@ -166,6 +166,13 @@ if ($result->num_rows > 0) {
     }
 ?>
 </select>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
 
 
 
@@ -178,13 +185,6 @@ if ($result->num_rows > 0) {
       </div>
     </div>
 
-<?php
-  }
-} else {
-  echo "0 results";
-}
-$conn->close();
-?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
   </body>
