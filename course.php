242bf19082421ee147d628e7786da84d043c6993
Emily Privett <eaprivett@ou.edu>
2022-10-25 17:14:33 -0500

    kjsjxox

--- course.php
+++ course.php
@@ -138,13 +138,7 @@ if ($result->num_rows > 0) {
         <input type="submit" value="Delete" class="btn btn-primary" onclick="return confirm('Are you sure?')" />
     </form>
     </tr>
-                    <?php
-                }
-                } else {
-                  echo "0 results";
-                }
-                $conn->close();
-                ?>
+           
 
   </tbody>
     </table>
@@ -176,6 +170,13 @@ if ($result->num_rows > 0) {
                 <input type="hidden" name="saveType" value="Add">
                 <button type="submit" class="btn btn-primary">Submit</button>
               </form>
+              <?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
           </div>
         </div>
       </div>
