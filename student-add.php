8e1b433bcdc64bc326749f6f15db81bb4f2b9186
Emily Privett <eaprivett@ou.edu>
2022-10-24 21:13:45 -0500

    please work

--- student-add.php
+++ student-add.php
@@ -50,10 +50,8 @@
     <input type="text" class="form-control" id="studentLastName" aria-describedby="nameHelp" name="sLName">
     <div id="nameHelp" class="form-text text-muted">Enter the student's last name.</div>
   </div>
-
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
   </body>
 </html>
