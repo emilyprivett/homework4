d9b0fbabc6af8ce6f2c2e71c87e1149b1a9347fa
Emily Privett <eaprivett@ou.edu>
2022-10-25 20:19:38 -0500

    try 2

--- student.php
+++ student.php
@@ -138,7 +138,7 @@ if ($result->num_rows > 0) {
                                     $selText ="";
                                   }
                             ?>
-                              <option value="<?=$SchoolRow['SchoolID']?>"<?=$selText?>><?=$SelectRow['StudentID']?></option>
+                              <option value="<?=$SchoolRow['SchoolID']?>"<?=$selText?>><?=$SelectRow['SchoolID']?></option>
                             <?php
                                   }
                             ?>
@@ -203,7 +203,7 @@ if ($result->num_rows > 0) {
                                     $selText ="";
                                   }
                             ?>
-                              <option value="<?=$SchoolRow['SchoolID']?>"<?=$selText?>><?=$SchoolRow['StudentID']?></option>
+                              <option value="<?=$SchoolRow['SchoolID']?>"<?=$selText?>><?=$SchoolRow['SchoolID']?></option>
                             <?php
                                   }
                             ?>
